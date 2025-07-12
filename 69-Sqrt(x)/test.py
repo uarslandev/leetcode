1ac63e550f861b9120e844f9d5ce27795d2a8a6c
Umut Arslan <umut_arslan@gmx.de>
2025-07-12 14:11:15 +0200

    wip

--- 69-Sqrt(x)/test.py
+++ 69-Sqrt(x)/test.py
@@ -11,6 +11,12 @@ class TestClass(unittest.TestCase):
     def test_2(self):
         self.assertEqual(self.s.mySqrt(8), 2)
 
+    def test_3(self):
+        self.assertEqual(self.s.mySqrt(0), 0)
+
+    def test_4(self):
+        self.assertEqual(self.s.mySqrt(1), 1)
+
 if __name__ == '__main__':
     unittest.main()
 
